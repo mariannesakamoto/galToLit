0943d1119e6d84351af98da7cfb2b335699d2b4b
ALUNO <ALUNO@10.85.198.7>
2024-06-04 16:40:24 -0300

    Commit project galToLit

---
+++ src/galToLit/GalToLit.java
@@ -0,0 +1,11 @@
+package galToLit;
+public class GalToLit {
+public static void main(String[] args) {
+// declare as variáveis
+double gallons=10;
+double liters=0;
+// adicione o cálculo aqui
+// exiba o resultado para o usuário
+System.out.println(gallons+" gallons equals "+liters+" liters");
+}
+}
